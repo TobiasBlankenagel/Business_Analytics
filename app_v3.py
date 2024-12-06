22651d7d407a396aa233d5a57f2675f9d429c78a
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-12-06 14:27:43 +0100

    2

--- app_v3.py
+++ app_v3.py
@@ -39,7 +39,7 @@ st.markdown("""
         text-align: center;
         font-size: 40px;
         font-weight: bold;
-        margin-top: 50px;
+        margin-top: 20px;
     }
 
     /* Beschreibungstext */
