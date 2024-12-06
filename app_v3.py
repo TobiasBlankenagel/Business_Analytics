c8cb0becc38b49de7f4a9fea65c56674afee297b
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-12-06 15:29:00 +0100

    w

--- app_v3.py
+++ app_v3.py
@@ -401,7 +401,7 @@ if st.button("🎯 Predict Attendance"):
 
     if team_info:
         max_capacity = team_info["max_capacity"]
-        predicted_attendance = (prediction / 100) * max_capacity  # Prozent der max_capacity
+        predicted_attendance = prediction* max_capacity  # Prozent der max_capacity
         attendance_30th = team_info["attendance_30th_percentile"]
         attendance_70th = team_info["attendance_70th_percentile"]
 
