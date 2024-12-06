3f8ddae121db1e8fb159aec464c69e764a4ff834
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-12-06 10:39:18 +0100

    2

--- app_v1.py
+++ app_v1.py
@@ -1,18 +1,19 @@
 import streamlit as st
-import datetime
-import pickle
-import numpy as np
 import pandas as pd
+import numpy as np
+import pickle
 import requests
+import datetime
 
 # Modelle laden
 def load_model(model_path):
     with open(model_path, 'rb') as file:
         return pickle.load(file)
 
-model_with_weather = load_model("finalized_model_with_weather.sav")
-model_without_weather = load_model("finalized_model_without_weather.sav")
+model_with_weather = load_model("./finalized_model_with_weather.sav")
+model_without_weather = load_model("./finalized_model_without_weather.sav")
 
+# Streamlit-Konfiguration
 st.set_page_config(
     page_title="Stadium Capacity Prediction",
     page_icon="🏟️",
@@ -22,18 +23,15 @@ st.set_page_config(
 st.title("🏟️ Stadium Capacity Prediction App")
 st.markdown("🎉⚽ This app predicts stadium capacity utilization.")
 
-# Eingabe für Teams und andere Parameter
-available_home_teams = [
-    'FC Sion', 'FC St. Gallen', 'FC Winterthur', 'FC Zürich',
-    'BSC Young Boys', 'FC Luzern', 'Lausanne-Sport', 'Servette FC',
-    'FC Basel', 'FC Lugano', 'Grasshoppers', 'Yverdon Sport'
-]
+# Eingrenzung der Teams und Wettbewerbe
+available_home_teams = ['FC Sion', 'FC St. Gallen', 'FC Winterthur', 'FC Zürich',
+                        'BSC Young Boys', 'FC Luzern', 'Lausanne-Sport', 'Servette FC',
+                        'FC Basel', 'FC Lugano', 'Grasshoppers', 'Yverdon Sport']
 available_away_teams = available_home_teams + ['Unknown']
-available_competitions = [
-    'Super League', 'UEFA Conference League', 'Swiss Cup',
-    'UEFA Europa League', 'UEFA Champions League'
-]
+available_competitions = ['Super League', 'UEFA Conference League', 'Swiss Cup', 
+                          'UEFA Europa League', 'UEFA Champions League']
 
+# Eingabefelder
 home_team = st.selectbox("Home Team:", available_home_teams)
 competition = st.selectbox("Competition:", available_competitions)
 
@@ -51,27 +49,9 @@ else:
 
 match_date = st.date_input("Match Date:", min_value=datetime.date.today())
 match_time = st.time_input(
-    "Match Time:",
-    value=datetime.time(15, 30),  # Standardwert auf 15:30 setzen
-    help="Select the match time in HH:MM format"
+    "Match Time:", value=datetime.time(15, 30), help="Select the match time in HH:MM format"
 )
-match_hour = match_time.hour
-
-# Stadionkoordinaten
-stadium_coordinates = {
-    'FC Sion': {'latitude': 46.233333, 'longitude': 7.376389},
-    'FC St. Gallen': {'latitude': 47.408333, 'longitude': 9.310278},
-    'FC Winterthur': {'latitude': 47.505278, 'longitude': 8.724167},
-    'FC Zürich': {'latitude': 47.382778, 'longitude': 8.504167},
-    'BSC Young Boys': {'latitude': 46.963056, 'longitude': 7.464722},
-    'FC Luzern': {'latitude': 47.035833, 'longitude': 8.310833},
-    'Lausanne-Sport': {'latitude': 46.537778, 'longitude': 6.614444},
-    'Servette FC': {'latitude': 46.1875, 'longitude': 6.128333},
-    'FC Basel': {'latitude': 47.541389, 'longitude': 7.620833},
-    'FC Lugano': {'latitude': 46.0225, 'longitude': 8.960278},
-    'Grasshoppers': {'latitude': 47.382778, 'longitude': 8.504167},
-    'Yverdon Sport': {'latitude': 46.778056, 'longitude': 6.641111}
-}
+match_hour = match_time.hour  # Holt nur die Stunde aus der Zeit
 
 # Wetterdaten abrufen
 def get_weather_data(latitude, longitude, match_date, match_hour):
@@ -81,6 +61,7 @@ def get_weather_data(latitude, longitude, match_date, match_hour):
         f"&hourly=temperature_2m,weathercode"
         f"&timezone=auto"
     )
+
     try:
         response = requests.get(api_url)
         response.raise_for_status()
@@ -88,7 +69,7 @@ def get_weather_data(latitude, longitude, match_date, match_hour):
         hourly_data = weather_data['hourly']
         temperature_at_match = hourly_data['temperature_2m'][match_hour]
         weather_code_at_match = hourly_data['weathercode'][match_hour]
-        
+
         if weather_code_at_match in [0]:
             weather_condition = "Clear or mostly clear"
         elif weather_code_at_match in [1, 2, 3]:
@@ -106,53 +87,57 @@ def get_weather_data(latitude, longitude, match_date, match_hour):
     except:
         return None, None
 
-# Wetterdaten für das Heimteam abrufen
+# Koordinaten der Stadien
+stadium_coordinates = {
+    'FC Sion': {'latitude': 46.233333, 'longitude': 7.376389},
+    'FC St. Gallen': {'latitude': 47.408333, 'longitude': 9.310278},
+    'FC Winterthur': {'latitude': 47.505278, 'longitude': 8.724167},
+    'FC Zürich': {'latitude': 47.382778, 'longitude': 8.504167},
+    'BSC Young Boys': {'latitude': 46.963056, 'longitude': 7.464722},
+    'FC Luzern': {'latitude': 47.035833, 'longitude': 8.310833},
+    'Lausanne-Sport': {'latitude': 46.537778, 'longitude': 6.614444},
+    'Servette FC': {'latitude': 46.1875, 'longitude': 6.128333},
+    'FC Basel': {'latitude': 47.541389, 'longitude': 7.620833},
+    'FC Lugano': {'latitude': 46.0225, 'longitude': 8.960278},
+    'Grasshoppers': {'latitude': 47.382778, 'longitude': 8.504167},
+    'Yverdon Sport': {'latitude': 46.778056, 'longitude': 6.641111}
+}
+
 if home_team and match_date and match_time:
     coordinates = stadium_coordinates[home_team]
-    temperature_at_match, weather_condition = get_weather_data(
-        coordinates['latitude'], coordinates['longitude'], match_date, match_hour
-    )
+    latitude = coordinates['latitude']
+    longitude = coordinates['longitude']
+    temperature_at_match, weather_condition = get_weather_data(latitude, longitude, match_date, match_hour)
 else:
     temperature_at_match, weather_condition = None, None
 
-# Zusätzliche Daten aus CSV
-league_data = pd.read_csv("new_league_data.csv")
+# Matchdaten abrufen
+league_data = pd.read_csv('new_league_data.csv')
 home_team_data = league_data[league_data['Unnamed: 0'] == home_team].iloc[0]
 away_team_data = league_data[league_data['Unnamed: 0'] == away_team].iloc[0]
 
-# Eingabedaten vorbereiten
-ranking_home_team = home_team_data['Ranking']
-ranking_away_team = away_team_data['Ranking']
-goals_scored_home_team = home_team_data['Goals_Scored_in_Last_5_Games']
-goals_conceded_home_team = home_team_data['Goals_Conceded_in_Last_5_Games']
-wins_home_team = home_team_data['Number_of_Wins_in_Last_5_Games']
+# Features vorbereiten
+input_features = {
+    'Time': match_hour,
+    'Ranking Home Team': home_team_data['Ranking'],
+    'Ranking Away Team': away_team_data['Ranking'],
+    'Temperature (°C)': temperature_at_match if temperature_at_match else 20,
+    'Month': match_date.month,
+    'Day': match_date.day,
+    'Goals Scored in Last 5 Games': home_team_data['Goals_Scored_in_Last_5_Games'],
+    'Goals Conceded in Last 5 Games': home_team_data['Goals_Conceded_in_Last_5_Games'],
+    'Number of Wins in Last 5 Games': home_team_data['Number_of_Wins_in_Last_5_Games'],
+}
 
-goals_scored_away_team = away_team_data['Goals_Scored_in_Last_5_Games']
-goals_conceded_away_team = away_team_data['Goals_Conceded_in_Last_5_Games']
-wins_away_team = away_team_data['Number_of_Wins_in_Last_5_Games']
-
-weekday = match_date.strftime('%A')
-month = match_date.month
-day = match_date.day
+# Dummy-coding für Features
+input_data = pd.DataFrame([input_features])
+input_data = pd.get_dummies(input_data)
 
 # Vorhersage
 if st.button("Predict Attendance"):
-    input_data = {
-        "Time": match_hour,
-        "Ranking Home Team": ranking_home_team,
-        "Ranking Away Team": ranking_away_team,
-        "Temperature (°C)": temperature_at_match or 20,  # Standardtemperatur
-        "Month": month,
-        "Day": day,
-        "Goals Scored in Last 5 Games": goals_scored_home_team,
-        "Goals Conceded in Last 5 Games": goals_conceded_home_team,
-        "Number of Wins in Last 5 Games": wins_home_team
-    }
-    # Dummy-Codierung einfügen
-    # Vorhersage starten
     if temperature_at_match is not None:
-        prediction = model_with_weather.predict(np.array([list(input_data.values())]))
+        prediction = model_with_weather.predict(input_data)[0]
     else:
-        prediction = model_without_weather.predict(np.array([list(input_data.values())]))
-    
-    st.success(f"Predicted Attendance Percentage: {prediction[0]:.2f}%")
+        prediction = model_without_weather.predict(input_data)[0]
+
+    st.success(f"Predicted Attendance Percentage: {prediction:.2f}%")
