c63adaf0b002978fc7536e52fbcb11e9cf7dbabe
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-12-09 10:05:36 +0100

    2

--- app_v4.py
+++ app_v4.py
@@ -10,6 +10,7 @@ import matplotlib
 ############################## MODELS & STREAMLIT CONFIGURATION ##############################
 
 # Function to load a model using pickle
+def load_model(model_path):
     with open(model_path, 'rb') as file:
         return pickle.load(file)
 
